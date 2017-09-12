7361801048e7706ab1bf592a18a25c16aeb58072
Sean M Hamlet <seanmhamlet@gmail.com>
2017-09-11 19:39:19 -0500

    Add grid system and style examples

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,8 +6,11 @@ var Main = (props) => {
       // remember you can only return one "root" element (in this case, a 'div')
       <div>
         <Nav/>
-        <h2>Main Component</h2>
-        {props.children}
+        <div className="row">
+          <div className="columns medium-6 large-4 small-centered">
+            {props.children}
+          </div>
+        </div>
       </div>
       // adding 'this.props.children' tells Main where to place children components
   );
