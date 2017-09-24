6f1c789af02a405857c35979ad01a738e9db652e
Sean M Hamlet <seanmhamlet@gmail.com>
2017-09-24 06:55:24 -0500

    Switch to scss

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
       Examples: 'app/components/Examples.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
       ErrorModal: 'app/components/ErrorModal.jsx',
-      applicationStyles: 'app/styles/app.css'
+      applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx'] // list of file extensions we want to process
   },
